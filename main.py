a8c7a641ee958aa9e79cdb7f192e4c0d30348b3f
Gleefre <varedif.a.s@gmail.com>
2020-04-13 12:35:07 +0300

    simple test fix 2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(token=tok.token)
 
 @bot.message_handler(commands=['start', 'help'])
 def rhi(message):
-    bot.send_message(chat_id=message.chat_id, text='Hi! Your k is ' + random.randint(1, 100))
+    bot.send_message(chat_id=message.chat.id, text='Hi! Your k is ' + random.randint(1, 100))
 
 
 bot.polling()
