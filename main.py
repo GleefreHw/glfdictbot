52d95292c6fc16493f946d469199a7c85a65c142
Gleefre <varedif.a.s@gmail.com>
2020-04-13 20:45:57 +0300

    simple test fix 4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 bot = telebot.TeleBot(token=tok.token)
 
 
-@bot.message_handler(func=True)
+@bot.message_handler(func=lambda x:True)
 def rhi(message):
     bot.send_message(chat_id=message.chat.id, text='Hi! Your k20 is ' + str(random.randint(1, 100)))
 
