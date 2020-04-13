9ff9d2fc5a5d698817d3a692121ed74fb0481113
Gleefre <varedif.a.s@gmail.com>
2020-04-13 20:44:42 +0300

    simple test upd 1

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import random
 bot = telebot.TeleBot(token=tok.token)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(func=True)
 def rhi(message):
-    bot.send_message(chat_id=message.chat.id, text='Hi! Your k is ' + str(random.randint(1, 100)))
+    bot.send_message(chat_id=message.chat.id, text='Hi! Your k20 is ' + str(random.randint(1, 100)))
 
 
 bot.polling()
