3d4b208079f5f6424feacd5ed19c6ee078678ff4
Gleefre <varedif.a.s@gmail.com>
2020-04-13 12:32:55 +0300

    simple test

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import tok
 import telebot
+import random
 
 
 bot = telebot.TeleBot(token=tok.token)
 
+
+@bot.message_handler(commands=['start', 'help'])
+def rhi(message):
+    bot.send_message(chat_id=message.chat_id, text='Hi! Your k is ' + random.randint(1, 100))
